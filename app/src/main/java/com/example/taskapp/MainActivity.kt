9a499b8f4fc2aaeaa75ae29d42d2d9b5e9ec55a7
Ahmed <yghnam334455@gmail.com>
2022-02-12 23:00:44 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar!!.hide()
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_main)
+    }
+}
